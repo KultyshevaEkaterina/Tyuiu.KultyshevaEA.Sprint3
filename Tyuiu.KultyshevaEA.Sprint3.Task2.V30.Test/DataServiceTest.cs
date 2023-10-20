1ece7fe26aaf97a36834f4aec1852e5dd4e5df7a
Kultysheva Ekaterina <kultysheva.ekaterina@gmail.com>
2023-10-20 20:58:12 +0500

    Task2

---
+++ Tyuiu.KultyshevaEA.Sprint3.Task2.V30.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KultyshevaEA.Sprint3.Task2.V30.Lib;
+
+namespace Tyuiu.KultyshevaEA.Sprint3.Task2.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+
+            DataService ds = new DataService();
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 11;
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+
+            double wait = 13051.340;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
