7f038782dbf5ee728ba812a7af7ff8a424d81904
Kultysheva Ekaterina <kultysheva.ekaterina@gmail.com>
2023-10-28 20:25:30 +0500

    Task7

---
+++ Tyuiu.KultyshevaEA.Sprint3.Task7.V18.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KultyshevaEA.Sprint3.Task7.V18.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KultyshevaEA.Sprint3.Task7.V18.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("233af41c-382e-45e6-ae9c-a1ea13c7869a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
